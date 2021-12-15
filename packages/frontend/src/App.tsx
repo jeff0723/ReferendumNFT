84d7c92889885cec33af513d94307ac94591935d
Jeffery.YH.Lin <jeffery.yh.lin@JefferyYHLindeMacBook-Pro.local>
2021-12-15 22:46:09 +0800

    fix some css

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,26 @@
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { Card, Form, Input, Layout, Typography, Button, Checkbox, Tooltip } from 'antd';
+import { Card, Form, Input, Layout, Typography, Button, Checkbox, Tooltip, Col, Row } from 'antd';
 import "antd/dist/antd.css";
 import { create } from 'ipfs-http-client';
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Account from './components/Account';
 import Chains from './components/Chains';
 import { useActiveWeb3React } from './hooks/web3';
+import { useMediaQuery } from 'react-responsive'
+
+
 import "./style.css";
 enum ImageStatus {
   NotUpload,
   Uploading,
   Uploaded
+}
+
+enum PageStatus {
+  Edit,
+  Uploading,
+  Finished
 }
 const { Header, Content, Footer } = Layout;
 const { Text } = Typography
@@ -19,14 +29,14 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    background: "#ffffff",
-    border: "1px solid #e7eaf3"
+    background: "linear-gradient(270deg, rgba(255,255,255,1) 0%, rgba(24,178,255,1)  100%)",
+    color: '#ffffff',
+    // border: "1px solid #e7eaf3"
   },
   contentBox: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    height: "100%",
     width: "50%",
     minWidth: '375px'
   },
@@ -40,6 +50,8 @@ const styles = {
     color: '#bfbfbf'
   }
 }
+
+
 const client = create({ url: 'https://ipfs.infura.io:5001/api/v0' })
 
 function App() {
@@ -48,7 +60,12 @@ function App() {
   const [imageURI, setImageURI] = useState<string>("");
   const [previewURL, setPreviewURL] = useState<string>("")
   const [imageStatus, setImageStatus] = useState(ImageStatus.NotUpload);
+  const [finish, setFinish] = useState(PageStatus.Edit);
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 992px)'
+  })
   console.log("App: ", chainId)
+  console.log("Desktop: ", isDesktop)
   const uploadButton = (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       {imageStatus === ImageStatus.Uploading ? <LoadingOutlined /> : <PlusOutlined />}
@@ -74,14 +91,14 @@ function App() {
   }
   const handleFinish = (values: any) => {
     console.log("Success: ", values)
-    console.log('form: ', form)
+
   }
   console.log("imageURI: ", imageURI)
   return (
-    <Layout style={{ height: "100vh", backgroundColor: "#ffffff" }}>
+    <Layout style={{ backgroundColor: "#ffffff" }}>
       <Header style={{ ...styles.header, position: 'fixed', zIndex: 1, width: '100%' }}>
         <div>
-          <Text style={{ color: '#1890ff', fontSize: '16px', fontWeight: 'bold' }}>Referendum</Text>
+          <Text style={{ color: '#ffffff', fontSize: '16px', fontWeight: 'bold' }}>Referendum</Text>
         </div>
         <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
           <Chains />
@@ -89,57 +106,68 @@ function App() {
 
         </div>
       </Header>
-      <Content style={{ display: 'flex', flexDirection: 'row', marginTop: '32px', padding: '16px', flexWrap: 'wrap' }}>
-        <div style={styles.contentBox}>
-          <div>
-            <Text style={styles.title}>紀念你的重要時刻</Text>
-            <br />
-            <Text style={styles.subtitle}>#Vote For NFT</Text>
-            <Text style={{ ...styles.subtitle, marginLeft: "5px" }}>#Democracy Token</Text>
-
-          </div>
-        </div>
-        <div style={styles.contentBox}>
-          <Card
-            // title={<Text>創建公投NFT</Text>}
-            style={{
-              borderRadius: '16px',
-              padding: '16px',
-              width: '50%',
-              minWidth: '375px'
-            }}>
-            <Form
-              form={form}
-              layout="vertical"
-              size={'large'}
-              onFinish={handleFinish}
-            >
-              <Form.Item name='name' label="名字" required tooltip="為您的專屬時刻起的名字吧">
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item name='description' label="你的心情" required tooltip="紀錄你當下的心情">
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="你的時刻" required tooltip="上傳一張照片紀念這個時刻">
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                  <input type="file" id="actual-btn" hidden onChange={handleUploadChange} />
-                  <label htmlFor="actual-btn">{imageStatus !== ImageStatus.Uploaded ? uploadButton : <img src={previewURL} alt='preview' height='100%' width="100%" style={{ maxHeight: '200px', maxWidth: '200px' }} />}</label>
-                </div>
-              </Form.Item>
-              <Form.Item name="gasfree" valuePropName="checked" >
-                <Tooltip title='支付gas fee的使用者將會多拿到一顆的民主精神代幣'>
-                  <Checkbox >免Gas Fee鑄造</Checkbox>
-                </Tooltip>
-              </Form.Item>
-              <div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
-                <Button type="primary" htmlType="submit" style={{ borderRadius: '16px', width: '100%' }}>
-                  鑄造
-                </Button>
+      <Content style={{ minHeight: 'calc(100vh - 48px)' }}>
+        <Row style={{ minHeight: 'calc(100vh - 48px)' }}>
+          <Col span={24} lg={12} >
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', textAlign: 'center', minHeight: '60vh' }}>
+              <div>
+                <Text style={styles.title}>紀念你的重要時刻</Text>
+              </div>
+              <div>
+                <Text style={styles.subtitle}>#Vote For NFT</Text>
+                <Text style={{ ...styles.subtitle, marginLeft: "5px" }}>#Democracy Token</Text>
               </div>
 
-            </Form>
-          </Card>
-        </div>
+            </div>
+          </Col>
+          <Col span={24} lg={12}>
+            <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              {!finish ?
+                <Card
+                  // title={<Text>創建公投NFT</Text>}
+                  style={{
+                    borderRadius: '16px',
+                    padding: '16px',
+                    width: '50%',
+                    minWidth: '330px'
+                  }}>
+                  <Form
+                    form={form}
+                    layout="vertical"
+                    size={'large'}
+                    onFinish={handleFinish}
+                  >
+                    <Form.Item name='name' label="名字" required tooltip="為您的專屬時刻起的名字吧">
+                      <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item name='description' label="你的心情" required tooltip="紀錄你當下的心情">
+                      <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item label="你的時刻" required tooltip="上傳一張照片紀念這個時刻">
+                      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <input type="file" id="actual-btn" hidden onChange={handleUploadChange} />
+                        <label htmlFor="actual-btn">{imageStatus !== ImageStatus.Uploaded ? uploadButton : <img src={previewURL} alt='preview' height='100%' width="100%" style={{ maxHeight: '200px', maxWidth: '200px' }} />}</label>
+                      </div>
+                    </Form.Item>
+                    <Form.Item name="gasfree" valuePropName="checked" >
+                      <Tooltip title='支付gas fee的使用者將會多拿到一顆的民主精神代幣'>
+                        <Checkbox >免Gas Fee鑄造</Checkbox>
+                      </Tooltip>
+                    </Form.Item>
+                    <div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
+                      <Button type="primary" htmlType="submit" style={{ borderRadius: '16px', width: '100%' }}>
+                        鑄造
+                      </Button>
+                    </div>
+
+                  </Form>
+                </Card> :
+                <Card></Card>
+              }
+            </div>
+          </Col>
+        </Row>
+
       </Content>
       <Footer>
 
