2d4d9d4a1dc879bb7e314899a4dfe3731ba4bc5a
Jeffery.YH.Lin <jeffery.yh.lin@JefferyYHLindeMacBook-Pro.local>
2021-12-15 12:00:19 +0800

    fix conflict

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,10 +3,7 @@ import "antd/dist/antd.css";
 import { Layout, Typography, Button, Modal } from 'antd'
 import Chains from './components/Chains'
 import Account from './components/Account'
-<<<<<<< HEAD
 import { useActiveWeb3React } from './hooks/web3';
-=======
->>>>>>> 39302bd24444ca16ff4c4bc77f034744dc925dfd
 import "./style.css";
 const { Header, Content, Footer } = Layout;
 const { Text } = Typography
@@ -19,11 +16,8 @@ const styles = {
   },
 }
 function App() {
-<<<<<<< HEAD
   const { chainId } = useActiveWeb3React();
   console.log("App: ", chainId)
-=======
->>>>>>> 39302bd24444ca16ff4c4bc77f034744dc925dfd
   return (
     <Layout style={{ height: "100vh", overflow: "auto" }}>
       <Header style={styles.header}>
